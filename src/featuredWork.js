94be1f7cd445adbfeb3177811e384fc3c0ff124e
pressR2 <36776929+pressR2@users.noreply.github.com>
2020-02-03 22:26:39 +0100

    feat: add slick script

--- src/featuredWork.js
+++ src/featuredWork.js
@@ -1,29 +1,35 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 class FeaturedWork extends React.Component {
     render() {
         let projectsImage = this.props.pics;
-        console.log(typeof(this.props.pics))
-       
+        var settings = {
+          dots: true,
+          infinite: true,
+          speed: 500,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        };
         return (
-            // <img src ="/image/project2.png"/>
-           <ol className ='container-projects'>
-               {this.props.projectsPics.map(function(pic) {
-                   console.log(pic.image);
-                   let oneImage = projectsImage(pic.image);
-                   console.log(oneImage);
+          <Slider {...settings}>
+           {this.props.projectsPics.map(function(pic) {
+              console.log(pic.image);
+              let oneImage = projectsImage(pic.image);
+               console.log(oneImage);
 
-                   return (
-                   <li key={pic.image}>
-                       <div className = 'box' style = {{
-                         backgroundImage: `url(${oneImage})`
-                       }}/>
-                       
-                   </li>
-               )})}
-           </ol>
-        )
-    }
+             return (
+              <div className = 'box' key={pic.image}>
+                 <img src={oneImage} alt=''></img>}
+               </div>
+           )})}
+          </Slider>
+        );
+      }
+    //     console.log(typeof(this.props.pics))
+ 
 }
 
 export default FeaturedWork;
