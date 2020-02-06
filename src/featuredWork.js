f3ae239fb31edc0000886309d4ec462f98404ab3
pressR2 <36776929+pressR2@users.noreply.github.com>
2020-02-06 20:45:40 +0100

    feat: post slider works fine with clickable chevron icons

--- src/featuredWork.js
+++ src/featuredWork.js
@@ -3,24 +3,41 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Leftchevron from "./logo1";
+import Rightchevron from "./logo2";
 
 class FeaturedWork extends React.Component {
+    constructor(props) {
+        super(props);
+        this.next = this.next.bind(this);
+        this.previous = this.previous.bind(this);
+    }
+
+    next() {
+        this.slider.slickNext();
+      }
+    previous() {
+        this.slider.slickPrev();
+      }
+
   render() {
     let projectsImage = this.props.pics;
     var settings = {
-      dots: true,
+    //   dots: true,
       slidesToShow: 3,
       slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
+    //   autoplay: true,
+    //   autoplaySpeed: 2000,
     };
     return (
       <div className="post-slider">
-        <svg className="logo">
-          <Leftchevron />
-        </svg>
+        <div className="icon-previous" onClick={this.previous}>
+          <Leftchevron/>
+        </div>
+        <div className="icon-next" onClick={this.next}>
+          <Rightchevron/>  
+        </div>
         <div className="post-wrapper">
-          <Slider {...settings}>
+          <Slider ref={c => (this.slider = c)} {...settings}>
             {this.props.projectsPics.map(function(pic) {
               console.log(pic.image);
               let oneImage = projectsImage(pic.image);
