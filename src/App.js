88884efc340e42ccf3a92fb3ac01a28d53af57c5
pressR2 <36776929+pressR2@users.noreply.github.com>
2020-02-06 19:26:28 +0100

    feat: adjust slider content style: add proper style to slider

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
 import React, { Component } from "react";
-import './App.css';
+import "./App.css";
 import FeaturedWork from "./featuredWork";
 import * as data from "./date.json";
 import ProjectsWebTechnologies from "./projectsWebTechnologies";
 import ProjectsPresentation from "./projectsPresentation";
+// import  Leftchevron from './logo1';
 
-const images = require.context('../image', true);
+const images = require.context("../image", true);
 
 class App extends Component {
-  
   render() {
-    
     // console.log(images);
     // const onePic = images('./project2.png');
     // console.log(onePic);
     return (
       <div>
         <main className="main-content">
-      <div>
-        <ProjectsPresentation/>
+          <div>
+            <ProjectsPresentation />
+          </div>
+        </main>
+        <article className="project-descripion">
+          <div>
+            <ProjectsWebTechnologies />
+          </div>
+        </article>
+        <nav className="menu">
+          <div>
+            <FeaturedWork projectsPics={data.images} pics={images} />
+          </div>
+        </nav>
       </div>
-      </main>
-      <article className="project-descripion">
-      <div>
-        <ProjectsWebTechnologies/>
-      </div>
-      </article>
-      <nav className="menu">
-      <div>
-        {/* <img src = {onePic} alt = 'kura'/> */}
-        <FeaturedWork projectsPics= {data.images} pics={images}/>
-      </div>
-      </nav>
-      </div>
-    )       
+    );
   }
 }
 
--- src/featuredWork.js
+++ src/featuredWork.js
@@ -2,34 +2,37 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Leftchevron from "./logo1";
 
 class FeaturedWork extends React.Component {
-    render() {
-        let projectsImage = this.props.pics;
-        var settings = {
-          dots: true,
-          infinite: true,
-          speed: 500,
-          slidesToShow: 1,
-          slidesToScroll: 1
-        };
-        return (
+  render() {
+    let projectsImage = this.props.pics;
+    var settings = {
+      dots: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    };
+    return (
+      <div className="post-slider">
+        <svg className="logo">
+          <Leftchevron />
+        </svg>
+        <div className="post-wrapper">
           <Slider {...settings}>
-           {this.props.projectsPics.map(function(pic) {
+            {this.props.projectsPics.map(function(pic) {
               console.log(pic.image);
               let oneImage = projectsImage(pic.image);
-               console.log(oneImage);
+              console.log(oneImage);
 
-             return (
-              <div className = 'box' key={pic.image}>
-                 <img src={oneImage} alt=''></img>}
-               </div>
-           )})}
+              return <img className="post" key={pic.image} src={oneImage} alt=""></img>;
+            })}
           </Slider>
-        );
-      }
-    //     console.log(typeof(this.props.pics))
- 
+        </div>
+      </div>
+    );
+  }
 }
 
 export default FeaturedWork;
