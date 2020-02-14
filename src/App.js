a9fd30fd4d809d0abc7c3faa6a2d30d9f9370e3d
pressR2 <36776929+pressR2@users.noreply.github.com>
2020-02-14 19:24:01 +0100

    feat: add MetaTags to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import FeaturedWork from "./featuredWork";
 import * as data from "./date.json";
 import ProjectsWebTechnologies from "./projectsWebTechnologies";
 import ProjectsPresentation from "./projectsPresentation";
+import MetaTags from "react-meta-tags";
 
 const images = require.context("../image", true);
 
@@ -27,42 +28,46 @@ class App extends Component {
     });
   }
 
-
   render() {
     return (
-      <div className="main">
-        <div className="flex-content">
-          <div className="main-wrapper">
-            <main className="main-content">
-              <div>
-                <ProjectsPresentation 
-                  selectVideo={this.state.projectVideo}
-                />
-              </div>
-            </main>
-            <article className="project-descripion">
-              <div>
-                <ProjectsWebTechnologies
-                  webTechnologies={this.state.projectWebIcons}
-                  selectDescription={this.state.projectDescription}
-                />
-              </div>
-            </article>
-          </div>
-          <nav className="menu">
-            <div>
-              <FeaturedWork
-                projects={data.projects}
-                pics={images}
-                selectProject={this.selectProject}
-                
-              />
+      <div className="wrapper">
+        <MetaTags>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </MetaTags>
+        <div className="main">
+          <div className="flex-content">
+            <div className="main-wrapper">
+              <main className="main-content">
+                <div>
+                  <ProjectsPresentation selectVideo={this.state.projectVideo} />
+                </div>
+              </main>
+              <article className="project-descripion">
+                <div>
+                  <ProjectsWebTechnologies
+                    webTechnologies={this.state.projectWebIcons}
+                    selectDescription={this.state.projectDescription}
+                  />
+                </div>
+              </article>
             </div>
-          </nav>
+            <nav className="menu">
+              <div>
+                <FeaturedWork
+                  projects={data.projects}
+                  pics={images}
+                  selectProject={this.selectProject}
+                />
+              </div>
+            </nav>
+          </div>
+          <footer className="footer">
+            <p className="footer p">photos by Marta</p>
+          </footer>
         </div>
-        <footer className="footer">
-          <p className="footer p">photos by Marta</p>
-        </footer>
       </div>
     );
   }
