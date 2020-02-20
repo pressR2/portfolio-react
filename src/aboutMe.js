4e755cd0244f537ffb30fd9f382aa7b2b0fef620
pressR2 <36776929+pressR2@users.noreply.github.com>
2020-02-20 21:44:10 +0100

    feat: add new component aboutMe.js with photo. Add some media query, style:change about me image in slider, refactor: pull tags outside data.json

---
+++ src/aboutMe.js
@@ -0,0 +1,29 @@
+import React from "react";
+import aboutMeImage from "./image2.PNG";
+
+class AboutMe extends React.Component {
+  render() {
+    return (
+      <div className="aboutMe-wrapper">
+        <div className="aboutMe-img">
+          <img src={aboutMeImage} alt="" />
+        </div>
+        <div className="aboutMe-content">
+          <hr></hr>
+          <h2>Hi, I'm Marta Drost</h2>
+          <h3>contact</h3>
+          <p>martadrost@protonmail.com</p>
+          <h3>github profile</h3>
+          <a href={"https://github.com/pressR2"}>github.com/pressR2</a>
+          <h3>codewars profile</h3>
+          <a href={"https://www.codewars.com/users/Marta_R2"}>
+            codewars.com/users/Marta_R2
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AboutMe;
+
