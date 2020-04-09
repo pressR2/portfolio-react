9730a68ae22447502123248d7255ca28bda55f5b
pressR2 <36776929+pressR2@users.noreply.github.com>
2020-04-10 00:10:04 +0200

    refactor: semantic elements

--- src/aboutMe.js
+++ src/aboutMe.js
@@ -4,23 +4,23 @@ import aboutMeImage from "./image2.PNG";
 class AboutMe extends React.Component {
   render() {
     return (
-      <article className="aboutMe-wrapper">
-        <div className="aboutMe-img">
+      <div className="aboutMe-wrapper">
+        <figure className="aboutMe-img">
           <img src={aboutMeImage} alt="Author's" />
-        </div>
-        <div className="aboutMe-content">
+        </figure>
+        <section className="aboutMe-content">
           <hr></hr>
           <h2>Hi, I'm Marta Drost</h2>
           <h3>contact</h3>
           <p>martadrost@protonmail.com</p>
           <h3>github profile</h3>
-          <a href={"https://github.com/pressR2"}>github.com/pressR2</a>
+          <a role="link" href={"https://github.com/pressR2"}>github.com/pressR2</a>
           <h3>codewars profile</h3>
-          <a href={"https://www.codewars.com/users/Marta_R2"}>
+          <a role="link" href={"https://www.codewars.com/users/Marta_R2"}>
             codewars.com/users/Marta_R2
           </a>
-        </div>
-      </article>
+        </section>
+      </div>
     );
   }
 }
