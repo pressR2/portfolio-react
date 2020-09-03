954ed456400d7f8da3cd1664e842600d4b20cd2d
pressR2 <36776929+pressR2@users.noreply.github.com>
2020-09-03 15:58:35 +0200

    Updates

---
+++ precache-manifest.96b2a65b1da8add92b239ecafec8713b.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e3ebe79b4161fdcff794df679360e9",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "5102891a9b3ffea64b1e",
+    "url": "/portfolio-react/static/css/2.4e001ec4.chunk.css"
+  },
+  {
+    "revision": "319b2ccda3217fe9ad0f",
+    "url": "/portfolio-react/static/css/main.176f925d.chunk.css"
+  },
+  {
+    "revision": "5102891a9b3ffea64b1e",
+    "url": "/portfolio-react/static/js/2.e26936a0.chunk.js"
+  },
+  {
+    "revision": "96aee9fb3ee9c9bc1cfe2d4b1be0dbb7",
+    "url": "/portfolio-react/static/js/2.e26936a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "319b2ccda3217fe9ad0f",
+    "url": "/portfolio-react/static/js/main.49852333.chunk.js"
+  },
+  {
+    "revision": "2cfe62a8b4dce97521d1",
+    "url": "/portfolio-react/static/js/runtime-main.757d4273.js"
+  },
+  {
+    "revision": "d049b31cc433ff3cbd21c47edacea929",
+    "url": "/portfolio-react/static/media/arcade-game.d049b31c.png"
+  },
+  {
+    "revision": "3be1c01a332422d2ae4f1e78c931bde4",
+    "url": "/portfolio-react/static/media/arcade-game2.3be1c01a.png"
+  },
+  {
+    "revision": "196fe17e087da187028dca34180e10f3",
+    "url": "/portfolio-react/static/media/author.196fe17e.png"
+  },
+  {
+    "revision": "bfd770095c7d88fc0e9b75c928ba6205",
+    "url": "/portfolio-react/static/media/author3.bfd77009.png"
+  },
+  {
+    "revision": "ff0217c1e5edcea46b8dc3a04d494acf",
+    "url": "/portfolio-react/static/media/feed-reader-screenshot.ff0217c1.png"
+  },
+  {
+    "revision": "6cfdd673fa24049ae9f5afc5553795c8",
+    "url": "/portfolio-react/static/media/feed-reader-testing.6cfdd673.png"
+  },
+  {
+    "revision": "c70b40c41a8db1ff34479bda00284b45",
+    "url": "/portfolio-react/static/media/feed-reader-testing2.c70b40c4.png"
+  },
+  {
+    "revision": "37ce81e620242f574945ed0efbacc1c2",
+    "url": "/portfolio-react/static/media/github-logo.37ce81e6.svg"
+  },
+  {
+    "revision": "7f3ac21407ca808ad36e7e179a5ae039",
+    "url": "/portfolio-react/static/media/iconfinder_codewars_4691329.7f3ac214.svg"
+  },
+  {
+    "revision": "795ea9774c61434ac710408ba8626894",
+    "url": "/portfolio-react/static/media/mail.795ea977.svg"
+  },
+  {
+    "revision": "0604614971e9fe43de55428e49f5f8c7",
+    "url": "/portfolio-react/static/media/memory-game.06046149.png"
+  },
+  {
+    "revision": "992a26318f7947251dab1a4a282c3bd1",
+    "url": "/portfolio-react/static/media/memory-game2.992a2631.png"
+  },
+  {
+    "revision": "8f52a68195718b63ecfd3a4403b6499f",
+    "url": "/portfolio-react/static/media/my-reads.8f52a681.png"
+  },
+  {
+    "revision": "5e8bcd0ce64e3533b38535826f14c2f5",
+    "url": "/portfolio-react/static/media/my-reads2.5e8bcd0c.png"
+  },
+  {
+    "revision": "ad66a49ae44741c2febc51e23c62392a",
+    "url": "/portfolio-react/static/media/neighborhood-map.ad66a49a.png"
+  },
+  {
+    "revision": "191b05170255a7759d5fefdacbac3a02",
+    "url": "/portfolio-react/static/media/neighborhood-map2.191b0517.png"
+  },
+  {
+    "revision": "d6e79dea494ab874a23741507ec8e83f",
+    "url": "/portfolio-react/static/media/pixel-art-maker2.d6e79dea.png"
+  },
+  {
+    "revision": "ac4b9a961d916950c7d8723fdb8cd6f2",
+    "url": "/portfolio-react/static/media/responsive-old-portfolio-site.ac4b9a96.png"
+  },
+  {
+    "revision": "9187f77455287e0d6617906fae8a54ff",
+    "url": "/portfolio-react/static/media/responsive-old-portfolio-site2.9187f774.png"
+  },
+  {
+    "revision": "7ff3315208f14a4d8660dfdf6ce901f2",
+    "url": "/portfolio-react/static/media/restaurant-reviews-app.7ff33152.png"
+  },
+  {
+    "revision": "f73ed4439e8e23dc0d02600e184553e5",
+    "url": "/portfolio-react/static/media/restaurant-reviews-app2.f73ed443.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfolio-react/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfolio-react/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfolio-react/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfolio-react/static/media/slick.f97e3bbf.svg"
+  }
+]);
