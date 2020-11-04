bea85eb3e4b714c3f9dd30d837a76969c6b7bcb0
pressR2 <36776929+pressR2@users.noreply.github.com>
2020-11-04 21:00:58 +0100

    refactor: use function component for AboutMe and FeedReader

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,57 +4,55 @@ import gitHubIcon from "../images/github-logo.svg";
 import mailIcon from "../images/mail.svg";
 import aboutMeSmallerImage from "../images/author3.png"
 
-class AboutMe extends React.Component {
-  render() {
-    return (
-      <section aria-label="Personal information">
-        <div className="author-wrapper">
-            <div className="author-image-box">
-              <img className="author-pic" src={aboutMeSmallerImage} alt="Author" />
+const AboutMe = () => { 
+  return (
+    <section aria-label="Personal information">
+      <div className="author-wrapper">
+          <div className="author-image-box">
+            <img className="author-pic" src={aboutMeSmallerImage} alt="Author" />
+          </div>
+        <div className="personal-information">
+          <hr className="orange-line"></hr>
+          <p className="author-name">
+            Hi, I'm Marta Drost
+          </p>
+          <div className="personal-data">
+            <div className="personal-item-wrapper">
+              <p className="personal-item-title">contact</p>
+              <div className="personal-item-icon">
+                <img src={mailIcon} alt="" />
+              </div>
             </div>
-          <div className="personal-information">
-            <hr className="orange-line"></hr>
-            <p className="author-name">
-              Hi, I'm Marta Drost
-            </p>
-            <div className="personal-data">
-              <div className="personal-item-wrapper">
-                <p className="personal-item-title">contact</p>
-                <div className="personal-item-icon">
-                  <img src={mailIcon} alt="" />
-                </div>
+            <p className="personal-item" >
+              <a className="personal-item-link" href={"mailto: martadrost@protonmail.com"} rel="noopener noreferrer" target="_blank">
+                martadrost@protonmail.com
+              </a></p>
+            <div className="personal-item-wrapper">
+              <p className="personal-item-title">github profile</p>
+              <div className="personal-item-icon">
+                <img src={gitHubIcon} alt="" />
               </div>
-              <p className="personal-item" >
-                <a className="personal-item-link" href={"mailto: martadrost@protonmail.com"} rel="noopener noreferrer" target="_blank">
-                  martadrost@protonmail.com
-                </a></p>
-              <div className="personal-item-wrapper">
-                <p className="personal-item-title">github profile</p>
-                <div className="personal-item-icon">
-                  <img src={gitHubIcon} alt="" />
-                </div>
+            </div>
+            <p className="personal-item">
+              <a className="personal-item-link" href={"https://github.com/pressR2"} rel="noopener noreferrer" target="_blank">
+                github.com/pressR2
+              </a></p>
+            <div className="personal-item-wrapper">
+              <p className="personal-item-title">codewars profile</p>
+              <div className="personal-item-icon">
+                <img src={codeWarsIcon} alt="" />
               </div>
+            </div>
               <p className="personal-item">
-                <a className="personal-item-link" href={"https://github.com/pressR2"} rel="noopener noreferrer" target="_blank">
-                  github.com/pressR2
-                </a></p>
-              <div className="personal-item-wrapper">
-                <p className="personal-item-title">codewars profile</p>
-                <div className="personal-item-icon">
-                  <img src={codeWarsIcon} alt="" />
-                </div>
-              </div>
-                <p className="personal-item">
-                  <a className="personal-item-link" href={"https://www.codewars.com/users/Marta_R2"} rel="noopener noreferrer" target="_blank">
-                    codewars.com/users/Marta_R2
-                  </a>
-                </p>
-            </div>
+                <a className="personal-item-link" href={"https://www.codewars.com/users/Marta_R2"} rel="noopener noreferrer" target="_blank">
+                  codewars.com/users/Marta_R2
+                </a>
+              </p>
           </div>
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
 }
 
 export default AboutMe;
--- src/components/FeedReader.js
+++ src/components/FeedReader.js
@@ -1,14 +1,12 @@
 import React from "react";
 import FeedReaderTests from "../images/feed-reader-screenshot.png";
 
-class FeedReader extends React.Component {
-  render() {
-    return (
-        <div>
-          <img className="feed-reader-screenShot" src={FeedReaderTests} alt="tests results" />
-        </div>
-    );
-  }
+const FeedReader = () => {
+  return (
+      <div>
+        <img className="feed-reader-screenShot" src={FeedReaderTests} alt="tests results" />
+      </div>
+  );
 }
 
 export default FeedReader;
